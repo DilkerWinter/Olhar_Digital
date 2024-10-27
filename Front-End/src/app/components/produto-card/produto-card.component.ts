7c59ebf49d99f98ebfc5aefa94557262464a0551
lbressanin1908 <lbressanin1908@gmail.com>
2024-10-27 01:40:56 -0400

    Header pronto

---
+++ Front-End/src/app/components/produto-card/produto-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './produto-card.component.html',
+  styleUrl: './produto-card.component.css'
+})
+export class ProdutoCardComponent {
+
+}
--- Front-End/src/app/shared/header/header.component.ts
+++ Front-End/src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import {faCartShopping, faBasketShopping} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -13,4 +13,5 @@ import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   faCart = faCartShopping;
+  faBasket = faBasketShopping;
 }
