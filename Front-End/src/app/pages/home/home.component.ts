d956504cae4b078d5f053909ce405d6f7c5ef811
Bruno Winter <brunohdwinter@gmail.com>
2024-10-30 00:40:08 -0400

    Criando container e card components e text tittle

--- Front-End/src/app/pages/home/home.component.ts
+++ Front-End/src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
-
+import { ProdutoCardComponent } from './components/produto-card/produto-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProdutoCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
