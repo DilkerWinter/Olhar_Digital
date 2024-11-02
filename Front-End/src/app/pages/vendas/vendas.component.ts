803c4249c27a30effa3c94b79d9846dad4082e1d
Bruno Winter <brunohdwinter@gmail.com>
2024-11-02 02:39:07 -0400

    Tela de Gerenciamento de Vendas Finalizada

--- Front-End/src/app/pages/vendas/vendas.component.ts
+++ Front-End/src/app/pages/vendas/vendas.component.ts
@@ -13,21 +13,65 @@ import { CommonModule } from '@angular/common';
 })
 export class VendasComponent implements OnInit {
   vendas: Venda[] = [];
-
+  valorTotal: number = 0;
+  valorTotalAnual: number = 0;
+  valorTotalMensal: number = 0; 
+  valorTotalPrefix: string = "R$";
+  valorTotalVendas: string = "";
+  valorTotalVendasMensal: string = "";
+  valorTotalVendasAnual: string = "";
   constructor(private vendaService: VendaService) {}
 
   ngOnInit(): void {
-    this.buscaTodasVendas(); 
+    this.buscaTodasVendas();
   }
 
   private buscaTodasVendas(): void {
     this.vendaService.getVendas().subscribe(
       (vendas: Venda[]) => {
         this.vendas = vendas; 
+        this.preencherResumoVendas();
       },
       error => {
         console.error('Error fetching vendas:', error);
       }
     );
   }
+
+  private preencherResumoVendas(): void {
+    this.valorTotal = this.vendas.reduce((total, venda) => total + venda.getValorTotal(), 0);
+    
+    this.valorTotalVendas = `${this.valorTotalPrefix} ${this.valorTotal.toFixed(2).replace('.', ',')}` || "R$ 0,00";
+
+    this.calcularTotalMensal();
+    this.calcularTotalAnual();
+  }
+
+  private calcularTotalMensal(): void {
+    const mesAtual = new Date().getMonth(); 
+    const anoAtual = new Date().getFullYear();
+
+    this.valorTotalMensal = this.vendas
+      .filter(venda => {
+        const dataVenda = new Date(venda.getDataVenda()); 
+        return dataVenda.getMonth() === mesAtual && dataVenda.getFullYear() === anoAtual;
+      })
+      .reduce((total, venda) => total + venda.getValorTotal(), 0);
+
+    this.valorTotalVendasMensal = `${this.valorTotalPrefix} ${this.valorTotalMensal.toFixed(2).replace('.', ',')}` || "R$ 0,00";
+  }
+
+  private calcularTotalAnual(): void {
+    const anoAtual = new Date().getFullYear();
+  
+    this.valorTotalAnual = this.vendas
+      .filter(venda => {
+        const dataVenda = new Date(venda.getDataVenda());
+        return dataVenda.getFullYear() === anoAtual;
+      })
+      .reduce((total, venda) => total + venda.getValorTotal(), 0);
+  
+    this.valorTotalVendasAnual = `${this.valorTotalPrefix} ${this.valorTotalAnual.toFixed(2).replace('.', ',')}` || "R$ 0,00";
+  }
+  
 }
